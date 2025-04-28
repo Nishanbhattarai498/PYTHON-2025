6bd838d10e967cdfd9d19ad4c27bf4f96235c6ac
nishan <your-email@example.com>
2025-04-28 15:44:50 +0545

    second commit

---
+++ dictionaries.py
@@ -0,0 +1,2 @@
+#dictionaries are used to store the data of the game
+# # dictionaries are used to store data in key-value pairs. Dictionaries are mutable, meaning they can be changed after creation. Dictionaries are created using curly braces {} and can contain items of different data types.
