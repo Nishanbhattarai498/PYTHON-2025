4407f24430ae948fe230a55b805d0bf1f71333c5
nishan <bhattarainisham@gmail.com>
2025-04-28 16:54:54 +0545

    commit

--- dictionaries.py
+++ dictionaries.py
@@ -1,2 +1,5 @@
 #dictionaries are used to store the data of the game
 # # dictionaries are used to store data in key-value pairs. Dictionaries are mutable, meaning they can be changed after creation. Dictionaries are created using curly braces {} and can contain items of different data types.
+# # Dictionaries are unordered, meaning the items do not have a defined order and that order may change over time. Dictionaries allow duplicate values.
+# # Dictionaries are indexed, meaning each item has a corresponding key, which is used to access the value. The keys must be unique and immutable (cannot be changed).
+# # Dictionaries are iterable, meaning you can loop through the items in a dictionary using a for loop or other iteration methods.
