d93567f7a48996614bf2f91294e4ffa86c19a81f
nishan <bhattarainisham@gmail.com>
2025-05-01 09:20:35 +0545

    Add copy.txt and implement file copying functionality in copy_file.py

---
+++ copy_file.py
@@ -0,0 +1,10 @@
+# copyfile()=copies content of the file 
+# Copy the contents of the file named src to a file named dst.
+# The destination file will be created if it does not exist, and it will be truncated if it does.
+#copy( ):copy file()+permission mode + destination can be a directory or file
+#copy2(): copy()+ copies metadata of the file
+
+
+import shutil
+import os
+shutil.copyfile('test.txt','copy.txt')
