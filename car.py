7b1807a719a9afa656a183dbd356efa08e504eb9
nishan <bhattarainisham@gmail.com>
2025-05-04 10:14:45 +0545

    Refactor Car class and update usage in oop_python.py

--- car.py
+++ car.py
@@ -1,16 +1,11 @@
 class Car:
-    make=None
-    model=None
-    year=None
-    color=None
+    # Class variable
+    wheels = 4 #class variable
+    
+    
 
     def __init__(self, make, model, year, color):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.color = color
-
-    def drive(self):
-        print("The car is driving.")
-    def stop(self):
-        print("The car has stopped.")
+        self.make = make #instance variable
+        self.model = model#instance variable
+        self.year = year #instance variable
+        self.color = color#instance variable
