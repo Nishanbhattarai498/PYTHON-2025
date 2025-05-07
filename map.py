68626e7f0c481f562bcd73bfd8514bdff1a397a5
nishan <bhattarainisham@gmail.com>
2025-05-07 17:17:29 +0545

    sixth

--- map.py
+++ map.py
@@ -4,5 +4,17 @@
 
 
 
-def sum():
-    return x+y
+store=[("shirt",400.00),
+       ("pant",200.00),
+       ("katttu",46.00),
+       ("peti",50.00)]
+
+
+to_euros=lambda data:(data[0],data[1]*135)
+
+
+store_euros=list(map(to_euros,store))
+
+for i in store_euros:
+    print(i)
+
