05f868a1804e332adbf0dc3766b3817943f33a3d
nishan <bhattarainisham@gmail.com>
2025-05-07 16:32:12 +0545

    third

---
+++ lambda.py
@@ -0,0 +1,17 @@
+#lambda parameters: expressions
+
+
+# double=lambda x:x*2
+# print(double(7))
+
+# multiply=lambda x,y :x * y
+# print(multiply(3,8))
+
+
+# add=lambda x,y,z:x+y+z
+# print(add(1,2,3))
+
+
+
+full=lambda first,second : first + " "+second
+print(full(input("enter first name::"),input("enter second name::")))
