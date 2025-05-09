bd143d2dde44af7b3a9bcdfd8a4a8c0541690810
nishan <bhattarainisham@gmail.com>
2025-05-09 08:30:40 +0545

    third

--- reduce.py
+++ reduce.py
@@ -1,1 +1,18 @@
-#reduce()=apply a function to an iterable 
+#reduce()=apply a function to an iterable  and reduce it to a single cumulative value.
+#performs function on first two element and repeat process until  i value remains
+#reduce(function,iterable)
+
+
+import functools
+letters=["H","E","L","L","O"]
+
+
+
+word=functools.reduce(lambda x,y:x+y,letters)
+print(word)
+
+
+factorial=[5,4,3,2,1] 
+
+fac=functools.reduce(lambda x,y:x*y,factorial)
+print(fac)
