ac5fa98585c72e28402c80f37591b06a5223e098
nishan <bhattarainisham@gmail.com>
2025-05-30 17:32:30 +0545

    Refactor regression performance metrics calculation by removing unnecessary code and streamlining imports

--- regression_performance.py
+++ regression_performance.py
@@ -1,15 +1,12 @@
-# Create X and y arrays
-X = sales_df.drop("sales", axis=1).values
-y = sales_df["sales"].values
+# Import mean_squared_error
+from sklearn.metrics import mean_squared_error  
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# Compute R-squared
+r_squared = reg.score(X_test, y_test)
 
-# Instantiate the model
-reg = LinearRegression()
+# Compute RMSE
+rmse = mean_squared_error(y_test, y_pred, squared=False)
 
-# Fit the model to the data
-reg.fit(X_train,y_train)
-
-# Make predictions
-y_pred = reg.predict(X_test)
-print("Predictions: {}, Actual Values: {}".format(y_pred[:2], y_test[:2]))
+# Print the metrics
+print("R^2: {}".format(r_squared))
+print("RMSE: {}".format(rmse))  
